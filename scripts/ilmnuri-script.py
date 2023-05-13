176f441396e618243be14ea7f1af42515388f43f
Cyberspyde <52118091+cyberspyde@users.noreply.github.com>
2023-05-14 01:13:03 +0900

    'all'

--- scripts/ilmnuri-script.py
+++ scripts/ilmnuri-script.py
@@ -2,11 +2,11 @@ import os
 from urllib.parse import quote
 
 folders = []
-
+files = []
 base_url = "https://github.com/cyberspyde/ilmnuri/raw/master/audio/Abdulloh%20domla/"
 
 for f in os.listdir('audio\\audio\\Abdulloh domla\\'):
-    files = []
+    
     folders.append(base_url+quote(f))
     for t in os.listdir('audio\\audio\\Abdulloh domla\\'+f):
         files.append(base_url+quote(f)+"/"+quote(t))
@@ -14,10 +14,7 @@ for f in os.listdir('audio\\audio\\Abdulloh domla\\'):
     os.makedirs(f'audio\\ADM\\{f}')
     with open(f'audio\\ADM\\{f}\\ADM.txt', 'w') as file:
         file.write('\n'.join(files))
-
-
-
-
+    
 
 #All the files in the folder
 # for f in os.listdir('audio\\Abdulloh domla\\'):
